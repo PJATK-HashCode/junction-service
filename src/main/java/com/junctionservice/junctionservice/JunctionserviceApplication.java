2bc52b934727ed564338fed6eaab42d533b6f91a
Jakub Merta <s14091@pjwstk.edu.pl>
2017-11-24 21:52:29 +0200

    init

---
+++ src/main/java/com/junctionservice/junctionservice/JunctionserviceApplication.java
@@ -0,0 +1,12 @@
+package com.junctionservice.junctionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunctionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunctionserviceApplication.class, args);
+	}
+}
