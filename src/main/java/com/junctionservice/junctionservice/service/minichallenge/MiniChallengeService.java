e6590bbc9bb7987dc0764537fe6afe2ea40b5058
Jakub Merta <s14091@pjwstk.edu.pl>
2017-11-25 18:57:21 +0200

    mini challenge service fixes

--- src/main/java/com/junctionservice/junctionservice/service/minichallenge/MiniChallengeService.java
+++ src/main/java/com/junctionservice/junctionservice/service/minichallenge/MiniChallengeService.java
@@ -1,7 +1,9 @@
 package com.junctionservice.junctionservice.service.minichallenge;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MiniChallengeService {
 
     @Autowired
