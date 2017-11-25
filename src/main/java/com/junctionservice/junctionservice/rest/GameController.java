2589458da01d51a1a22f22f9f2e6205706ce5347
Jakub Merta <s14091@pjwstk.edu.pl>
2017-11-25 10:27:35 +0200

    start new game service

--- src/main/java/com/junctionservice/junctionservice/rest/GameController.java
+++ src/main/java/com/junctionservice/junctionservice/rest/GameController.java
@@ -1,24 +1,34 @@
 package com.junctionservice.junctionservice.rest;
 
+import com.junctionservice.junctionservice.model.Competition;
 import com.junctionservice.junctionservice.model.Game;
 import com.junctionservice.junctionservice.model.response.MatchResponse;
-import jdk.jfr.ContentType;
+import com.junctionservice.junctionservice.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sun.security.jca.GetInstance;
-
-import javax.xml.ws.Response;
 
 @RestController
 public class GameController {
 
-    public GameController() {
-        Game.getGame();
+    GameService gameService;
+
+    public Game game;
+
+    @Autowired
+    public GameController(GameService gameService) {
+        game = Game.getGame();
+        this.gameService = gameService;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ResponseEntity<MatchResponse> startGame(@RequestParam()){
-        return;
-    }
+    public ResponseEntity<MatchResponse> startGame(@RequestBody Competition competition){
+        MatchResponse matchResponse = gameService.startNewGame(competition,game);
 
+
+
+
+        return new ResponseEntity(matchResponse, HttpStatus.OK);
+    }
 }
--- src/main/java/com/junctionservice/junctionservice/service/GameService.java
+++ src/main/java/com/junctionservice/junctionservice/service/GameService.java
@@ -1,9 +1,31 @@
 package com.junctionservice.junctionservice.service;
 
+import com.junctionservice.junctionservice.model.Competition;
+import com.junctionservice.junctionservice.model.Game;
+import com.junctionservice.junctionservice.model.response.MatchResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
+
+
+    private static long gameIdentifier = 0;
+
+    public GameService() {
+
+
+    }
+
+    public MatchResponse startNewGame(Competition competition, Game game){
+        MatchResponse matchResponse = new MatchResponse();
+        competition.getAdmin().setId();
+
+        competition.setGameCode(String.valueOf(gameIdentifier));
+
+        game.competition.put(gameIdentifier,competition);
+        gameIdentifier++;
+        return matchResponse;
+    }
 
 
 }
