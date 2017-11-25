c75ce57f9f781f1e6e2de4d2eee638d8a8d19015
Jakub Merta <s14091@pjwstk.edu.pl>
2017-11-25 13:28:48 +0200

    time to prepare data structure for games

---
+++ src/main/java/com/junctionservice/junctionservice/model/CompetitionModes/TempNameFirstCompetition.java
@@ -0,0 +1,8 @@
+package com.junctionservice.junctionservice.model.CompetitionModes;
+
+import java.util.List;
+
+public class TempNameFirstCompetition {
+    private List<String> listOfQuestions;
+    private List<String> listOfAnswers;
+}
